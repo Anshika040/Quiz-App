64fbed9dd2e3f7d69acb3ed2f4fe0f8b39e0e810
anshika040 <anshilashukla3466@gamil.com>
2024-06-10 15:09:49 +0530

    Quiz App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Quiz from "./Components/Quiz/Quiz";
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Quiz> </Quiz>
+    </>
+  );
+};
+
+export default App;
